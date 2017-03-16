06514431207dbfe3241fa18f8145b776e0499523
xiaos <753623217@qq.com>
2017-03-16 15:19:46 +0800

    commit

--- routes/controllers.js
+++ routes/controllers.js
@@ -2,13 +2,15 @@
  * Created by xiaos on 16/11/14.
  */
 const util = require('../lib/util')
+const postRouter = require('./post').router
+const userRouter = require('./user').router
 
 const controller = (app)=> {
     app.get('/index',  (req, res) => {
         return res.render('index')
     })
-    app.use('/post',require('./post').router)
-    app.use('/user', require('./user').router)
+    app.use('/post',postRouter)
+    app.use('/user',userRouter)
 }
 
 module.exports = controller;
